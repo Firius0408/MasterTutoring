3cf518fb15b125fddfd658704c805012012a264b
Brian Cheng <brian.cheng@ucla.edu>
2020-11-13 17:17:58 -0800

    Integration with MongoDB

--- backend/backend.js
+++ backend/backend.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
 
-require('./src/database');
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+mongoose.connect('mongodb://127.0.0.1:27017/test', {
+    useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen(PORT, function () {
     console.log(`Server Listening on ${PORT}`);
