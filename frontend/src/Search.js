c7bb5a1b494957e83333c16ca03fe4846c6cf103
Lance <lanceding2001@gmail.com>
2020-11-22 21:13:17 -0800

    Update Home

---
+++ frontend/src/Search.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import Select from 'react-select'
+
+const options = [
+    { value: 'math', label: 'Math' },
+    { value: 'english', label: 'English' },
+    { value: 'science', label: 'Science' },
+  ];
+
+  class Search extends Component {
+    options = [
+        { value: 'math', label: 'Math' },
+        { value: 'english', label: 'English' },
+        { value: 'science', label: 'Science' },
+      ];
+    state = {
+        selectedOption: null,
+      };
+      handleChange = selectedOption => {
+        this.setState({ selectedOption });
+        console.log(`Option selected:`, selectedOption);
+      };
+      render() {
+        const { selectedOption } = this.state;
+     
+        return (
+          <Select
+            value={selectedOption}
+            onChange={this.handleChange}
+            options={options}
+          />
+        );
+      }
+  }
+  export default Search;
