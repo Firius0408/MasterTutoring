f81a2ab470cef2cb7f120f6d2835c1d9ed20abbf
Brian Cheng <brian.cheng@ucla.edu>
2020-11-30 23:31:25 -0800

    Reinstall cors to backend to allow cross origin resource sharing

--- backend/backend.js
+++ backend/backend.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 4000;
 
+app.use(cors());
 app.use(express.json());
 
 const uri = 'mongodb://127.0.0.1:27017/people'
