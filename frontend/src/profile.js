cecb1f1486baf959b28cb3497db703d95286439e
Brian Cheng <brian.cheng@ucla.edu>
2020-12-06 21:02:05 -0800

    Profile page implemented

--- frontend/src/profile.js
+++ frontend/src/profile.js
@@ -4,7 +4,13 @@ import { Redirect } from 'react-router-dom';
 class Profile extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            id: '',
+            firstName: '',
+            lastName: '',
+            phoneNumber: 0,
+            email: '',
+        };
     }
 
     componentDidMount() {
@@ -22,7 +28,16 @@ class Profile extends React.Component {
                 headers: {'Content-Type': 'application/json'}
             })
             .then(response => response.json())
-            .then(data => this.setState(data))
+            .then(data => {
+                this.setState({
+                    id: data._id,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    phoneNumber: data.phoneNumber,
+                    email: data.email,
+                });
+                console.log(data);
+            })
             .catch(err => console.error('Error:', err));
         })
         .catch(err => console.error('Error:', err));
@@ -34,7 +49,21 @@ class Profile extends React.Component {
     }
 
     handleSubmit = (event) => {
-
+        const { email, confirmEmail } = this.state;
+        if (confirmEmail && email !== confirmEmail) {
+            alert('Emails do not match');
+            return;
+        }
+        const apiURL = "http://localhost:4000/user/update/" + this.state.id;
+        fetch(apiURL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(this.state)
+        })
+        .then(response => response.json())
+        .then(data => console.log('Success', data))
+        .catch(err => console.error('Error:', err));
+        event.preventDefault();
     }
 
     render() {
@@ -42,11 +71,11 @@ class Profile extends React.Component {
           <div>
             {this.props.loggedIn ? 
                 <form onSubmit={this.handleSubmit}>
-                <input type='text' name='firstName' value={this.state.firstName} onChange={this.handleChange} />
-                <input type='text' name='lastName' value={this.state.lastName} onChange={this.handleChange} />
-                <input type='text' name='phoneNumber' value={this.state.phoneNumber} onChange={this.handleChange} />
-                <input type='email' name='email' value={this.state.email} onChange={this.handleChange} />
-                <input type='username' name='userName' value={this.state.userName} onChange={this.handleChange} />
+                <input type='text' name='firstName' placeholder='First Name' value={this.state.firstName} onChange={this.handleChange} />
+                <input type='text' name='lastName' placeholder='Last Name' value={this.state.lastName} onChange={this.handleChange} />
+                <input type='text' name='phoneNumber' placeholder='Phone Number' value={this.state.phoneNumber} onChange={this.handleChange} />
+                <input type='email' name='email' placeholder='Email' value={this.state.email} onChange={this.handleChange} />
+                <input type='email' name='confirmEmail' placeholder='Confirm Email' value={this.state.confirmEmail} onChange={this.handleChange} />
                 <input type='password' name='password' placeholder='password' value={this.state.password} onChange={this.handleChange} />
                 <input type='submit' value='Edit Profile' />
             </form>
